cf08e83404546b275e2d93c4f4a64f3c73426c3a
sowmya <sowmyareddy645@gmail.com>
2018-06-27 13:34:33 +0530

    Added Login UI
    - Added LoginViewController and Its respective swift file.
    - Made the LoginViewController as Initial View Controller.

---
+++ AllSheNeeds/AllSheNeeds/UserValidation/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  AllSheNeeds
+//
+//  Created by Sowmya Chada on 27/06/18.
+//  Copyright © 2018 Ramprasad A. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
