4168b26f273240be71188aaed8b18e99dae0755c
Ramprasad A <ramprasad179@gmail.com>
2018-06-27 13:26:56 +0530

    Added UserValidation Module

---
+++ AllSheNeeds/AllSheNeeds/UserValidation/SignupViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SignupViewController.swift
+//  AllSheNeeds
+//
+//  Created by Ramprasad A on 27/06/18.
+//  Copyright © 2018 Ramprasad A. All rights reserved.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
